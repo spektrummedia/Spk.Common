04bfdc35787ad31f9d37178c671ab03427943c39
Gabriel Robert <g.robert092@gmail.com>
2017-09-18 11:00:17 -0400

    useful set of datetime extensions to convert to epoch time

---
+++ src/Spk.Common.Helpers/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Spk.Common.Helpers.DateTime
+{
+    public static class DateTimeExtensions
+    {
+        private static readonly System.DateTime Epoch = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+        /// <summary>
+        ///     Returns a Unix Epoch time given a <see cref="System.DateTime" />
+        /// </summary>
+        /// <param name="dt">The <see cref="System.DateTime" /> to convert.</param>
+        /// <returns></returns>
+        public static long ToEpochTime(this System.DateTime dt)
+        {
+            return (long) (dt - Epoch).TotalSeconds;
+        }
+
+        /// <summary>
+        ///     Returns a Unix Epoch time if given a value, and null otherwise.
+        /// </summary>
+        /// <param name="dt">The <see cref="System.DateTime" /> to convert.</param>
+        /// <returns></returns>
+        public static long? ToEpochTime(this System.DateTime? dt)
+        {
+            return dt.HasValue ? (long?) ToEpochTime(dt.Value) : null;
+        }
+    }
+}
---
+++ src/Spk.Common.Tests.Helpers/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using Spk.Common.Helpers.DateTime;
+
+namespace Spk.Common.Tests.Helpers.DateTime
+{
+    public class DateTimeExtensions
+    {
+        [Test]
+        public void ToEpochTime_ShouldReturnValidValue()
+        {
+            var date = new System.DateTime(2014, 1, 1, 6, 0, 0);
+            Assert.That(date.ToEpochTime(), Is.EqualTo(1388556000));
+        }
+    }
+}
