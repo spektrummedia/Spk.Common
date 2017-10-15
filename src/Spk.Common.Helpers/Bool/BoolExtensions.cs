658c9db6e74a8a239e1e82c561912e46b671e3d8
Gabriel Robert <g.robert092@gmail.com>
2017-10-15 11:42:12 -0400

    ToNullableBool method implementation.

---
+++ src/Spk.Common.Helpers/Bool/BoolExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Spk.Common.Helpers.Bool
+{
+    public static class BoolExtensions
+    {
+        /// <summary>
+        /// Returns a nullable bool given a <see cref="string" />.
+        /// "True", "true" or "TRUE" will returns true.
+        /// "False", "false" or "FALSE" will returns false.
+        /// "Anything", "null" or null will returns null.
+        /// </summary>
+        /// <param name="nullableBoolean">The <see cref="string" /> to convert.</param>
+        /// <returns><see cref="bool?" /></returns>
+        public static bool? ToNullableBool(this string nullableBoolean)
+        {
+            bool value;
+            if (Boolean.TryParse(nullableBoolean, out value))
+            {
+                return value;
+            }
+            return null;
+        }
+    }
+}
---
+++ test/Spk.Common.Tests.Helpers/Bool/BoolExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Spk.Common.Helpers.Bool;
+using Xunit;
+
+namespace Spk.Common.Tests.Helpers.Bool
+{
+    public class BoolExtensionsTests
+    {
+        [Fact]
+        public void ToNullableBool_ShouldReturnTrue_WhenStringTrue()
+        {
+            Assert.True("true".ToNullableBool());
+            Assert.True("True".ToNullableBool());
+            Assert.True("TRUE".ToNullableBool());
+            Assert.True(true.ToString().ToNullableBool());
+        }
+
+        [Fact]
+        public void ToNullableBool_ShouldReturnFalse_WhenJavascriptTrue()
+        {
+            Assert.False("false".ToNullableBool());
+            Assert.False("False".ToNullableBool());
+            Assert.False("FALSE".ToNullableBool());
+            Assert.False(false.ToString().ToNullableBool());
+        }
+
+        [Fact]
+        public void ToNullableBool_ShouldReturNull()
+        {
+            Assert.Null(BoolExtensions.ToNullableBool(null));
+        }
+
+        [Fact]
+        public void ToNullableBool_ShouldReturNull_WhenBadValue()
+        {
+            Assert.Null("test".ToNullableBool());
+        }
+    }
+}
