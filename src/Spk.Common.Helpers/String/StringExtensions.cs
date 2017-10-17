3f02897f17e3743695fada5f9aa2586bc8115b85
Gabriel Robert <g.robert092@gmail.com>
2017-10-17 10:32:39 -0400

    Multiple string extensions using the Convert class

--- src/Spk.Common.Helpers/String/StringExtensions.cs
+++ src/Spk.Common.Helpers/String/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace Spk.Common.Helpers.String
 {
@@ -45,5 +46,33 @@ namespace Spk.Common.Helpers.String
         /// <param name="s">The URL-encoded string to decode.</param>
         /// <returns></returns>
         public static string UrlDecode(this string s) => s.HasValue() ? WebUtility.UrlDecode(s) : s;
+
+        /// <summary>
+        /// Convert a string to integer using the Convert.ToInt32 method.
+        /// </summary>
+        /// <param name="s">The string to convert.</param>
+        /// <returns></returns>
+        public static int ToInt32(this string s) => Convert.ToInt32(s);
+
+        /// <summary>
+        /// Convert a string to float using the Convert.ToSingle method.
+        /// </summary>
+        /// <param name="s">The string to convert.</param>
+        /// <returns></returns>
+        public static float ToFloat(this string s) => Convert.ToSingle(s);
+
+        /// <summary>
+        /// Convert a string to decimal using the Convert.ToDecimal method.
+        /// </summary>
+        /// <param name="s">The string to convert.</param>
+        /// <returns></returns>
+        public static decimal ToDecimal(this string s) => Convert.ToDecimal(s);
+
+        /// <summary>
+        /// Convert a string to boolean using the Convert.ToBoolean method.
+        /// </summary>
+        /// <param name="s">The string to convert.</param>
+        /// <returns></returns>
+        public static bool ToBoolean(this string s) => Convert.ToBoolean(s);
     }
 }
--- test/Spk.Common.Tests.Helpers/String/StringExtensionsTests.cs
+++ test/Spk.Common.Tests.Helpers/String/StringExtensionsTests.cs
@@ -4,21 +4,20 @@ using Xunit;
 namespace Spk.Common.Tests.Helpers.String
 {
     /// <summary>
-    /// These tests exist just to make sure they call the right API.
+    ///     These tests exist just to make sure they call the right API.
     /// </summary>
     public class StringExtensionsTests
     {
         [Fact]
-        public void IsNullorEmpty_ShouldReturnTrue_WhenNull()
+        public void HtmlDecode_ShouldDecodeProperly()
         {
-            string test = null;
-            Assert.True(test.IsNullOrEmpty());
+            Assert.Equal("<p>Hi!</p>", "&lt;p&gt;Hi!&lt;/p&gt;".HtmlDecode());
         }
 
         [Fact]
-        public void IsNullorEmpty_ShouldReturnTrue_WhenEmpty()
+        public void HtmlEncode_ShouldEncodeProperly()
         {
-            Assert.True("".IsNullOrEmpty());
+            Assert.Equal("&lt;p&gt;Hi!&lt;/p&gt;", "<p>Hi!</p>".HtmlEncode());
         }
 
         [Fact]
@@ -28,31 +27,53 @@ namespace Spk.Common.Tests.Helpers.String
         }
 
         [Fact]
-        public void HtmlEncode_ShouldEncodeProperly()
+        public void IsNullorEmpty_ShouldReturnTrue_WhenEmpty()
         {
-            var result = "<p>Hi!</p>".HtmlEncode();
-            Assert.Equal("&lt;p&gt;Hi!&lt;/p&gt;", result);
+            Assert.True("".IsNullOrEmpty());
         }
 
         [Fact]
-        public void HtmlDecode_ShouldDecodeProperly()
+        public void IsNullorEmpty_ShouldReturnTrue_WhenNull()
         {
-            var result = "&lt;p&gt;Hi!&lt;/p&gt;".HtmlDecode();
-            Assert.Equal("<p>Hi!</p>", result);
+            string test = null;
+            Assert.True(test.IsNullOrEmpty());
         }
 
         [Fact]
-        public void UrlEncode_ShouldEncodeProperly()
+        public void ToDecimal_ShouldConvertProperly()
         {
-            var result = "test string".UrlEncode();
-            Assert.Equal("test+string", result);
+            Assert.Equal(42.01m, "42.01".ToDecimal());
+        }
+
+        [Fact]
+        public void ToFloat_ShouldConvertProperly()
+        {
+            Assert.Equal(42.01f, "42.01".ToFloat());
+        }
+
+        [Fact]
+        public void ToInt32_ShouldConvertProperly()
+        {
+            Assert.Equal(42, "42".ToInt32());
+        }
+
+        [Fact]
+        public void ToBoolean_ShouldConvertProperly()
+        {
+            Assert.True("true".ToBoolean());
+            Assert.False("false".ToBoolean());
         }
 
         [Fact]
         public void UrlDecode_ShouldDecodeProperly()
         {
-            var result = "test+string".UrlDecode();
-            Assert.Equal("test string", result);
+            Assert.Equal("test string", "test+string".UrlDecode());
+        }
+
+        [Fact]
+        public void UrlEncode_ShouldEncodeProperly()
+        {
+            Assert.Equal("test+string", "test string".UrlEncode());
         }
     }
 }
