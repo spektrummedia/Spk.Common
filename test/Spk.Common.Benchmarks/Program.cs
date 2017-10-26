c61f469790364a8142a4551519b40fa33bc672ad
Gabriel Robert <g.robert092@gmail.com>
2017-10-26 07:48:19 -0400

    Spk.Common.Benchmarks needs to be console app and .NET framework

---
+++ test/Spk.Common.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spk.Common.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
